2aee0f3fe6fa85276a745c93e8c40b9fa247c8d5
Hadrien Caron <hadrien@barbow.(none)>
2013-04-16 21:46:39 +0200

    starting unit testing

---
+++ libraries/unitTest/session/sessionTest.cc
@@ -0,0 +1,17 @@
+#define BOOST_TEST_MODULE MyTest
+#include <boost/test/unit_test.hpp>
+
+BOOST_AUTO_TEST_CASE( my_test )
+{
+  //my_class test_object( "qwerty" );
+
+  BOOST_CHECK( true );
+}
+
+BOOST_AUTO_TEST_CASE( my_test_2 )
+{
+  //my_class test_object( "qwerty" );
+
+  BOOST_CHECK( true );
+}
+
